913c309d4b93eea73cba9736b119e93972d902a4
IgorKrol <igork08@gmail.com>
2019-05-03 22:30:21 +0300

    bullpgia function complete

--- calculate.cpp
+++ calculate.cpp
@@ -2,17 +2,33 @@
 #include <string.h>
 
 #include "calculate.hpp"
-
+using namespace std;
 string bullpgia::calculateBullAndPgia(string codeMaker, string codeBreaker){
-	uint bulls(0), pgia(0);
-	////// VARIABLES: char arrays - codeMaker and codeBreaker //////
-	char cMaker[codeMaker.length()];
-	strcpy(cMaker, codeMaker.c_str());
-	char cBreaker[codeBreaker.length()];
-	strcpy(cBreaker, codeBreaker.c_str());
 	
+	uint bull=0,pgia=0;
+	////// CALC PGIA //////
+	string pgiaMaker="",pgiaBreaker="";
 	
-
-
-	return codeBreaker;
+	for (int i = 0; i < codeMaker.length(); i++)
+	{
+		if (codeMaker[i] == codeBreaker[i]){
+				bull++;
+		}
+		else{
+			pgiaMaker+=codeMaker[i];
+			pgiaBreaker+=codeBreaker[i];
+		}
+	}
+	for (int i = 0; i < pgiaMaker.length(); i++)
+	{
+		for (int j = 0; j < pgiaBreaker.length(); j++)
+		{
+			if(pgiaMaker[i]==pgiaBreaker[j]){
+				pgia++;
+				break;
+			}
+		}
+	}
+	string res = to_string(bull)+","+to_string(pgia);
+	return res;
 }
