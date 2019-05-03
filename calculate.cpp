f7393c7254a2dba77696a5f51cb0d4a7cce69a60
IgorKrol <igork08@gmail.com>
2019-05-03 20:55:19 +0300

    bullpgia - start

--- calculate.cpp
+++ calculate.cpp
@@ -1,8 +1,18 @@
 
-#include <string>
+#include <string.h>
 
 #include "calculate.hpp"
 
-string bullpgia::calculateBullAndPgia(string choice, string guess){
-	return guess;
+string bullpgia::calculateBullAndPgia(string codeMaker, string codeBreaker){
+	uint bulls(0), pgia(0);
+	////// VARIABLES: char arrays - codeMaker and codeBreaker //////
+	char cMaker[codeMaker.length()];
+	strcpy(cMaker, codeMaker.c_str());
+	char cBreaker[codeBreaker.length()];
+	strcpy(cBreaker, codeBreaker.c_str());
+	
+	
+
+
+	return codeBreaker;
 }
